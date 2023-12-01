9892d03729b07b9473327cf1786bca77a4d9b458
Terrence <terrence.liu@alumni.ubc.ca>
2023-11-30 17:53:03 -0800

    Add bootstrap buttons and alert for failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:error] = 'Invalid username or password. Please try again.'
       redirect_to '/login'
     end
   end
